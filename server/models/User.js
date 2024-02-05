81df8f93efb17cf987bfa08be846f2b7ddca5b95
Arthur Bridges <arthurbridges08@yahoo.com>
2024-02-04 18:43:38 -0600

    just staging changes of the userSchema

---
+++ server/models/User.js
@@ -0,0 +1,72 @@
+import { Schema, model } from "mongoose";
+import bcrypt from "bcrypt";
+
+class UserClass {
+  constructor({ email, password, userName, firstName, lastName }) {
+    if (new.target === UserClass) {
+      throw new Error("Cannot directly call class !!");
+    }
+    this.email = email;
+    this.password = password;
+    this.userName = userName;
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  welcomeUser() {
+   console.log(`Welcome ${this.userName}`);
+  }
+}
+
+const hashNum = 15;
+
+function createUserSchema() {
+  return new Schema({
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+      userName: {
+      type: String,
+      minlength: , // TODO: discuss with team on length allowed
+      maxlength: ,// TODO: discuss with team on length allowed
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      minlength: , // TODO: discuss with team on length allowed
+      maxlength: , // TODO: discuss with team on length allowed
+      required: true,
+    },
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+  });
+}
+
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')){ 
+    return next();
+  }
+
+  try {
+    const salt = await bcrypt.genSalt(hashNum);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } 
+  catch (err) {
+    next(err);
+  }
+});
+
+const userSchema = createUserSchema();
+const User = model("User", userSchema);
+
+export default User;
