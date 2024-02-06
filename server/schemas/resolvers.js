a23a547a9a6841d48dfad15d23fd61798f938085
Tatum Sterling <tatum.sterling29@gmail.com>
2024-02-05 20:52:23 -0600

    updated resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,8 +1,75 @@
-import {Quiz } from '../models/Quiz';
-import {User} from '../models/User';
+import  Quiz  from "../models/Quiz";
+import User  from "../models/User";
+import Profile  from '../models/Profile';
+import { signToken, AuthenticationError } from "../utils/auth";
 
 const resolvers = {
-    Query: {
-        
-    }
-}
+  Query: {
+    // find all quizzez 
+    quizzes: async () => {
+      return Quiz.find().sort({ createdAt: -1 });
+    },
+    //find one quiz
+    quiz: async (parent, { quizId }) => {
+      return Quiz.findOne({ _id: quizId });
+    },
+    // find all profiles
+    profiles: async () => {
+      return Profile.find();
+    },
+    // find one profile by id
+    profile: async (parent, { profileId }) => {
+      return Profile.findOne({ _id: profileId });
+    },
+    // By adding context to our query, we can retrieve the logged in user without specifically searching for them
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return Profile.findOne({ _id: context.user._id });
+      }
+      throw AuthenticationError;
+    },
+  },
+  Mutation: {
+    // create a new quiz
+    addQuiz: async (parent, { quizQuestions, quizAnswers }) => {
+      return Quiz.create({ quizQuestion, quizAnswer });
+    },
+    // delete quiz
+    removeQuiz: async (parent, { quizId }) => {
+      return Quiz.findOneAndDelete({ _id: quizId });
+    },
+    // create new profile SIGN UP
+    addProfile: async (parent, { name, email, password }) => {
+      const profile = await Profile.create({ name, email, password });
+      const token = signToken(profile);
+
+      return { token, profile };
+    },
+    // LOGIN
+    login: async (parent, { email, password }) => {
+      const profile = await Profile.findOne({ email });
+
+      if (!profile) {
+        throw AuthenticationError;
+      }
+
+      const correctPw = await profile.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw AuthenticationError;
+      }
+
+      const token = signToken(profile);
+      return { token, profile };
+    },
+    // delete profile
+    removeProfile: async (parent, args, context) => {
+      if (context.user) {
+        return Profile.findOneAndDelete({ _id: context.user._id });
+      }
+      throw AuthenticationError;
+    },
+  },
+};
+
+module.exports = resolvers;
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,15 +1,59 @@
-import jwt from 'jsonwebtoken';
+// import jwt from 'jsonwebtoken';
 
-const authMiddleware = (req, res, next) => {
-  try {
-    const token = req.header('Authorization').replace('Bearer ', '');
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    // Add user finding logic here using decoded data (usually user ID)
-    // For example: req.user = await User.findById(decoded.id);
-    next();
-  } catch (error) {
-    res.status(401).send({ error: 'Please authenticate.' });
-  }
+// const authMiddleware = (req, res, next) => {
+//   try {
+//     const token = req.header('Authorization').replace('Bearer ', '');
+//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+//     // Add user finding logic here using decoded data (usually user ID)
+//     // For example: req.user = await User.findById(decoded.id);
+//     next();
+//   } catch (error) {
+//     res.status(401).send({ error: 'Please authenticate.' });
+//   }
+// };
+
+// export { authMiddleware };
+
+
+
+const { GraphQLError } = require('graphql');
+const jwt = require('jsonwebtoken');
+
+const secret = 'mysecretssshhhhhhh';
+const expiration = '2h';
+
+module.exports = {
+  AuthenticationError: new GraphQLError('Could not authenticate user.', {
+    extensions: {
+      code: 'UNAUTHENTICATED',
+    },
+  }),
+  authMiddleware: function ({ req }) {
+    // allows token to be sent via req.body, req.query, or headers
+    let token = req.body.token || req.query.token || req.headers.authorization;
+
+    // We split the token string into an array and return actual token
+    if (req.headers.authorization) {
+      token = token.split(' ').pop().trim();
+    }
+
+    if (!token) {
+      return req;
+    }
+
+    // if token can be verified, add the decoded user's data to the request so it can be accessed in the resolver
+    try {
+      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      req.user = data;
+    } catch {
+      console.log('Invalid token');
+    }
+
+    // return the request object so it can be passed to the resolver as `context`
+    return req;
+  },
+  signToken: function ({ email, name, _id }) {
+    const payload = { email, name, _id };
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
 };
-
-export { authMiddleware };
