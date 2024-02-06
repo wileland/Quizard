a12e3d520843146e8f2dca36ae850363da9648a7
wileland <wileland7@gmail.com>
2024-02-06 10:01:46 -0600

    Add Apollo Client and user query

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,21 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Login from "./pages/Login.jsx";
+import { useQuery } from "@apollo/client"; // Import the useQuery hook
+import { gql } from "@apollo/client"; // Import the gql template literal
+import { GET_USERS } from "./queries/userQueries"; // Import your user query
 
 function App() {
+  // Use the useQuery hook to fetch data
+  const { loading, error, data } = useQuery(GET_USERS);
+
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route
+          path="/"
+          element={<Home data={data} />} // Pass the fetched user data to Home component
+        />
         <Route path="/login" element={<Login />} />
       </Routes>
     </>
@@ -16,3 +25,4 @@ function App() {
 }
 
 export default App;
+
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,25 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
 import App from "./App.jsx";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+// Initialize Apollo Client with the correct GraphQL server URI
+const client = new ApolloClient({
+  uri: 'http://localhost:3001/graphql', // Updated URI to match your server
+  cache: new InMemoryCache(),
+});
+
+// Create a root element to render
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ApolloProvider>
   </React.StrictMode>,
 );
