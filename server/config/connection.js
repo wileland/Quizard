c32b9de529c2687ff9508dfa07d63fe542ea91fd
wileland <wileland7@gmail.com>
2024-02-06 21:43:18 -0600

    a

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 const connectDB = async () => {
-  mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/');
+  mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/quizzardDB',);
 }
 
 
