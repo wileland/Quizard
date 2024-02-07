12c74c315f66b2141fcbc3d95b3f4bdbd377f107
wileland <wileland7@gmail.com>
2024-02-06 22:02:14 -0600

    heroku push

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 const connectDB = async () => {
-  mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/QuizardDB');
+  mongoose.connect(process.env.MONGODB_URI);
 };
 
 
