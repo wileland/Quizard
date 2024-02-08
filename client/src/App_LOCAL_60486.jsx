685a0adb987b331eb4f73af306025e32b232b885
Arthur Bridges <arthurbridges08@yahoo.com>
2024-02-07 22:01:43 -0600

    Created file for quizData

---
+++ client/src/App_LOCAL_60486.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Login from "./pages/Login.jsx";
+import { setContext } from "@apollo/client/link/context";
+import "./App.css";
+
+// Create an HTTP link that connects to the GraphQL server.
+// const httpLink = createHttpLink({
+//   uri: '/graphql',
+// });
+
+// Middleware to attach the authentication token to requests
+const authLink = setContext((_, { headers }) => {
+  // Get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // Return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    }
+  };
+});
+
+// Instantiate Apollo Client with the created httpLink and cache
+const client = new ApolloClient({
+  uri: '/graphql',
+  // link: authLink.concat(httpLink), // Chain it with the httpLink
+  cache: new InMemoryCache(),
+});
+
+function App() {
+  // ApolloProvider should be the outermost component
+  return (
+    <ApolloProvider client={client}>
+      {/* No need to use <Router> again if it's already used in index.js/main.jsx */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </ApolloProvider>
+  );
+}
+
+export default App;
---
+++ server/controllers/quizData.js
@@ -0,0 +1,2 @@
+import Quiz from "../models/Quiz.js";
+import mongoose from "mongoose";
