cdd16880562cfe0ce7b502998dee3f638a405ee7
Arthur Bridges <arthurbridges08@yahoo.com>
2024-02-08 03:23:40 -0600

    Implemented the querie
    implemented the query QUERY_QUIZZES within dashboard.jsx to fetch quiz data.

--- client/src/pages/dashboard.jsx
+++ client/src/pages/dashboard.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import { useQuery, gql } from "@apollo/client";
+import { QUERY_QUIZZES } from "../utils/queries";
 
 const Dashboard = () => {
+  const { loading, error, data } = useQuery(QUERY_QUIZZES);
+
   return (
     <main>
       {/*Maybe header here with the display name, TODO: abstract header into a 
     component then import said component within the pages*/}
       <ul>
         {/*fetch the data here, map it with it's id*/}
-        {/*data.map(({ id }) => (
-        <li key={id} >  </li>
-      ))*/}
+        {data.quizzes.map(({ _id }) => (
+          <li key={_id}></li>
+        ))}
       </ul>
     </main>
   );
