5ee3ef58dd1b245114d359287b5e04ac6367dee6
CodePro-ABridges <arthurbridges08@yahoo.com>
2024-02-11 16:02:49 -0600

    Added game schema

--- server/models/Game.js
+++ server/models/Game.js
@@ -1,2 +1,21 @@
 //need pin, hostId, boolean value for gameActive
 // also will need game data
+import { Schema, SchemaTypes, model } from "mongoose";
+
+const gameSchema = new Schema({
+  hostId: {
+    type: String,
+    required: true,
+  },
+  pin: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  gameLive: Boolean,
+  gameData: SchemaTypes.Mixed,
+});
+
+const Game = model("Game", gameSchema);
+
+export default Game;
