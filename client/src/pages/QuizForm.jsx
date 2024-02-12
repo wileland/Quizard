a58ea5b35380add49e3a329b8e7495a3434beb0f
CodePro-ABridges <arthurbridges08@yahoo.com>
2024-02-11 20:45:42 -0600

    Debugging :(

--- client/src/pages/QuizForm.jsx
+++ client/src/pages/QuizForm.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import QuestionForm from "../components/QuestionForm.jsx";
 import io from "socket.io-client";
+import authService from "../utils/auth"; // Adjust the path as per your structure
 
 const socket = io("http://localhost:3000");
 
@@ -35,9 +36,12 @@ const QuizForm = () => {
   };
 
   const submitQuiz = () => {
+    // Use authService to get the current user's profile which includes the ID
+    const userProfile = authService.getProfile();
     const quizData = {
       title,
       questions,
+      hostId: userProfile._id, // Use the _id from the user's profile as the hostId
     };
     socket.emit("newQuiz", quizData);
     setTitle("");
